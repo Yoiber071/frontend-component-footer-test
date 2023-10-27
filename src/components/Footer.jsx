f6747dc071622a0a73bc4ec0af372c27f8308893
Yoiber Beitar <yoiber.beitar@edunext.co>
2023-10-27 11:46:28 -0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'jquery/dist/jquery.slim';
+import 'popper.js/dist/popper';
+import {injectIntl, intlShape} from '@edx/frontend-platform/i18n';
+import {sendTrackEvent} from '@edx/frontend-platform/analytics';
+import {ensureConfig} from '@edx/frontend-platform/config';
+import {AppContext} from '@edx/frontend-platform/react';
+
+import messages from './Footer.messages';
+import LanguageSelector from './LanguageSelector';
+
+ensureConfig([
+    'LMS_BASE_URL',
+    'LOGO_TRADEMARK_URL',
+], 'Footer component');
+
+const EVENT_NAMES = {
+    FOOTER_LINK: 'edx.bi.footer.link',
+};
+
+class SiteFooter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.externalLinkClickHandler = this.externalLinkClickHandler.bind(this);
+    }
+
+    externalLinkClickHandler(event) {
+        const label = event.currentTarget.getAttribute('href');
+        const eventName = EVENT_NAMES.FOOTER_LINK;
+        const properties = {
+            category: 'outbound_link',
+            label,
+        };
+        sendTrackEvent(eventName, properties);
+    }
+
+    render() {
+        const {
+            supportedLanguages,
+            onLanguageSelected,
+            logo,
+            intl,
+        } = this.props;
+        const showLanguageSelector = supportedLanguages.length > 0 && onLanguageSelected;
+        const {config} = this.context;
+
+        return (
+            <footer
+                role="contentinfo"
+                className="footer border-top py-3 px-4 footer-basic"
+            >
+                <div className="">
+                    {/*<a*/}
+                    {/*    className="d-block"*/}
+                    {/*    href={config.LMS_BASE_URL}*/}
+                    {/*    aria-label={intl.formatMessage(messages['footer.logo.ariaLabel'])}*/}
+                    {/*>*/}
+
+                        <div className="social">
+                            <a href="#"><i className="icon ion-social-instagram"></i></a>
+                            <a href="#"><i className="icon ion-social-twitter"></i></a>
+                            <a href="#"><i className="icon ion-social-facebook"></i></a>
+                        </div>
+                        <ul className="list-inline">
+                            <li className="list-inline-item"><a href="#">Home</a></li>
+                            <li className="list-inline-item"><a href="#">Services</a></li>
+                            <li className="list-inline-item"><a href="#">About</a></li>
+                            <li className="list-inline-item"><a href="#">Terms</a></li>
+                            <li className="list-inline-item"><a href="#">Privacy Policy</a></li>
+                        </ul>
+                        <p className="copyright">eduNEXT © {new Date().getFullYear()}</p>
+                        <img
+                            style={{maxHeight: 45}}
+                            src={logo || config.LOGO_TRADEMARK_URL}
+                            alt={intl.formatMessage(messages['footer.logo.altText'])}
+                        />
+                    {/*</a>*/}
+                    <div className="flex-grow-1"/>
+                    {showLanguageSelector && (
+                        <LanguageSelector
+                            options={supportedLanguages}
+                            onSubmit={onLanguageSelected}
+                        />
+                    )}
+                </div>
+            </footer>
+        );
+    }
+}
+
+SiteFooter.contextType = AppContext;
+
+SiteFooter.propTypes = {
+    intl: intlShape.isRequired,
+    logo: PropTypes.string,
+    onLanguageSelected: PropTypes.func,
+    supportedLanguages: PropTypes.arrayOf(PropTypes.shape({
+        label: PropTypes.string.isRequired,
+        value: PropTypes.string.isRequired,
+    })),
+};
+
+SiteFooter.defaultProps = {
+    logo: undefined,
+    onLanguageSelected: undefined,
+    supportedLanguages: [],
+};
+
+export default injectIntl(SiteFooter);
+export {EVENT_NAMES};
